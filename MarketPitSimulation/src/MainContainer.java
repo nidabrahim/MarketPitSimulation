eaf174bc51e44663573ee248e5a3728593d18b59
NIDABRAHIM Youssef <youssef.nidabrahim@um5s.net.ma>
2018-01-30 12:01:09 +0100

    init project

---
+++ MarketPitSimulation/src/MainContainer.java
@@ -0,0 +1,27 @@
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.util.ExtendedProperties;
+import jade.util.leap.Properties;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.ControllerException;
+
+public class MainContainer {
+
+	public static void main(String[] args) {
+		
+		Runtime runtime = Runtime.instance();
+		Properties properties = new ExtendedProperties();
+		properties.setProperty(Profile.GUI, "true");
+		ProfileImpl profileImpl = new ProfileImpl(properties);
+		AgentContainer agentContainer = runtime.createMainContainer(profileImpl);
+		try {
+			agentContainer.start();
+		} catch (ControllerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
